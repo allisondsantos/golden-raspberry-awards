ff0edd7aef59dbc49939b23c8918c9e5977e42d4
allisondiego <allisondiego@gmail.com>
2023-05-22 20:51:53 -0300

    feat: create movie entity

---
+++ src/Golden.Raspberry.Awards.WebAPI/Domain/Entities/Movie.cs
@@ -0,0 +1,34 @@
+﻿using Texo.Teste.WebAPI.Domain.Models;
+
+namespace Texo.Teste.WebAPI.Domain.Entities
+{
+    public class Movie
+    {
+
+        public Movie(ImportMovieModel model)
+        {
+            Year = model.Year;
+            Title = model.Title;
+            Studio = model.Studio;
+            Producer = model.Producer;
+            Winner = model.Winner;
+        }
+
+        protected Movie()
+        {
+
+        }
+
+        public long Id { get; set; }
+        
+        public int Year { get; private set; }
+
+        public string Title { get; private set; } = string.Empty;
+
+        public string Studio { get; private set; } = string.Empty;
+
+        public string Producer { get; private set; } = string.Empty;
+
+        public bool Winner { get; private set; }
+    }
+}
---
+++ src/Golden.Raspberry.Awards.WebAPI/Domain/Models/ImportMovieModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Texo.Teste.WebAPI.Domain.Models
+{
+    public class ImportMovieModel
+    {
+        public int Year { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Studio { get; set; } = string.Empty;
+
+        public string Producer { get; set; } = string.Empty;
+
+        public bool Winner { get; set; }
+    }
+}
