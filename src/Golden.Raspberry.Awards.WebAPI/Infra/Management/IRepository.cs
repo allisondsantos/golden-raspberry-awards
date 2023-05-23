f18080d6079dfe5551c9dc1e6e5ad3ed7da2342c
allisondiego <allisondiego@gmail.com>
2023-05-22 22:52:58 -0300

    feat: create movie repository

---
+++ src/Golden.Raspberry.Awards.WebAPI/Infra/Management/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Golden.Raspberry.Awards.WebAPI.Infra.Management
+{
+    public interface IRepository<TEntity>
+    {
+        void Add(TEntity entity);
+
+        IQueryable<TEntity> GetAll();
+    }
+}
---
+++ src/Golden.Raspberry.Awards.WebAPI/Infra/Management/Repository.cs
@@ -0,0 +1,25 @@
+﻿using Golden.Raspberry.Awards.WebAPI.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Golden.Raspberry.Awards.WebAPI.Infra.Management
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(IDbContext context)
+        {
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _ = _dbSet.Add(entity);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet;
+        }
+    }
+}
