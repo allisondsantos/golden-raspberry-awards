fe531f9b25d30ac5ae1c8a37fe2c852282050125
allisondiego <allisondiego@gmail.com>
2023-05-22 22:54:11 -0300

    feat: add movie migration

---
+++ src/Golden.Raspberry.Awards.WebAPI/Infra/Migrations/20230523013738_CreateTableMovies.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Golden.Raspberry.Awards.WebAPI.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    year = table.Column<int>(type: "INTEGER", nullable: false),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    studio = table.Column<string>(type: "TEXT", nullable: false),
+                    producer = table.Column<string>(type: "TEXT", nullable: false),
+                    winner = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movies", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "movies");
+        }
+    }
+}
