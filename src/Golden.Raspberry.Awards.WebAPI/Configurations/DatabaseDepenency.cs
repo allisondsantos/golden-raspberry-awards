5f5f7d691a62b06bbc52a5a6ab2a9f59edd81229
allisondiego <allisondiego@gmail.com>
2023-05-23 22:28:06 -0300

    refactor: segregate database service dependency

---
+++ src/Golden.Raspberry.Awards.WebAPI/Configurations/DatabaseDepenency.cs
@@ -0,0 +1,56 @@
+﻿using Golden.Raspberry.Awards.WebAPI.Infra.Context;
+using Golden.Raspberry.Awards.WebAPI.Infra.Management;
+using Golden.Raspberry.Awards.WebAPI.Infra.Seeding;
+using Microsoft.EntityFrameworkCore;
+
+namespace Golden.Raspberry.Awards.WebAPI.Configurations
+{
+    public static class DatabaseDepenency
+    {
+        public static IServiceCollection ConfigureDatabaseDepenency(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connection = configuration.GetSection("ConnectionString:Sqlite");
+
+            if (connection is null)
+            {
+                throw new Exception("The database connection has not been set up.");
+            }
+
+            services.AddDbContext<SqliteContext>(options =>
+                options.UseSqlite(connection.Value)
+            );
+
+            services.AddScoped<IDbContext, SqliteContext>();
+            return services;
+        }
+
+        public static IHost ApplyMigrations(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<SqliteContext>();
+                if (context is null)
+                {
+                    throw new Exception("Invalid context.");
+                }
+
+                context.Database.Migrate();
+            }
+
+            return host;
+        }
+
+        public static async Task SeedDataAsync(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var unitOfWork = services.GetService<IUnitOfWork>();
+
+            var bancoSeed = new MovieSeed(unitOfWork);
+            await bancoSeed.SeedDataAsync();
+        }
+    }
+
+}
--- src/Golden.Raspberry.Awards.WebAPI/Program.cs
+++ src/Golden.Raspberry.Awards.WebAPI/Program.cs
@@ -1,6 +1,7 @@
-using Golden.Raspberry.Awards.WebAPI.Infra.Context;
-using Microsoft.EntityFrameworkCore;
-using System;
+using Golden.Raspberry.Awards.WebAPI.Commands;
+using Golden.Raspberry.Awards.WebAPI.Configurations;
+using Golden.Raspberry.Awards.WebAPI.Infra.Management;
+using Golden.Raspberry.Awards.WebAPI.Infra.Seeding;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,18 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureDatabaseDepenency(builder.Configuration);
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ISeedDatabase, MovieSeed>();
+builder.Services.AddTransient<AwardRangeCommandHandler>();
 
-var connection = builder.Configuration.GetSection("ConnectionString:Sqlite");
-
-if (connection is null)
-{
-    throw new Exception("The database connection has not been set up.");
-}
-
-builder.Services.AddDbContext<SqliteContext>(options =>
-    options.UseSqlite(connection.Value)
-);
 var app = builder.Build();
+app.ApplyMigrations();
+await app.SeedDataAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,16 +27,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-using (var reportScope = app.Services.CreateScope())
-{
-    var context = reportScope.ServiceProvider.GetService<SqliteContext>();
-    if (context is null)
-    {
-        throw new Exception("Invalid context.");
-    }
-
-    context.Database.Migrate();
-}
 
 app.UseHttpsRedirection();
 
