fa352e2422758a5c4c544a3b9768783a36aa363c
allisondiego <allisondiego@gmail.com>
2023-05-23 23:48:57 -0300

    test: created integration test for MovieController

---
+++ tests/Golden.Raspberry.Awards.Tests/Integration/MovieControllerTest.Data.cs
@@ -0,0 +1,62 @@
+﻿using Golden.Raspberry.Awards.WebAPI.Commands;
+using Golden.Raspberry.Awards.WebAPI.Domain.Dto;
+using Xunit;
+
+namespace Golden.Raspberry.Awards.Tests.Integration
+{
+    public partial class MovieControllerTest
+    {
+        public static TheoryData<AwardRangeResponse> TheoryGetAwardRangeShouldBeReturnCorrectContent
+            => new()
+            {
+                {
+                    new AwardRangeResponse()
+                    {
+                        Min = GetMinProducerAward(), 
+                        Max = GetMaxProducerAward(), 
+                    }
+                }
+            };
+
+        private static IEnumerable<ProducerAwardDto> GetMinProducerAward()
+        {
+            return new List<ProducerAwardDto>()
+            {
+                {
+                    new ProducerAwardDto()
+                    {
+                        Producer = "Yoram Globus and Menahem Golan",
+                        Interval = 1,
+                        PreviousWin =  1986,
+                        FollowingWin = 1987,
+                    }
+                },
+                {
+                    new ProducerAwardDto()
+                    {
+                        Producer = "Wyck Godfrey, Stephenie Meyer and Karen Rosenfelt",
+                        Interval = 1,
+                        PreviousWin = 2011,
+                        FollowingWin = 2012,
+                    }
+                },
+            }.AsEnumerable();
+        }
+
+        private static IEnumerable<ProducerAwardDto> GetMaxProducerAward()
+        {
+            return new List<ProducerAwardDto>()
+            {
+                {
+                    new ProducerAwardDto()
+                    {
+                        Producer = "Jerry Weintraub",
+                        Interval = 9,
+                        PreviousWin = 1980,
+                        FollowingWin = 1989,
+                    }
+                },
+            }.AsEnumerable();
+        }
+    }
+}
--- tests/Golden.Raspberry.Awards.Tests/Integration/MovieControllerTest.cs
+++ tests/Golden.Raspberry.Awards.Tests/Integration/MovieControllerTest.cs
@@ -1,12 +1,40 @@
+using FluentAssertions;
+using Golden.Raspberry.Awards.WebAPI.Commands;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text.Json;
 using Xunit;
 
 namespace Golden.Raspberry.Awards.Tests.Integration;
 
-public class UnitTest1
+public partial class MovieControllerTest
+    : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public void Test1()
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public MovieControllerTest(WebApplicationFactory<Program> factory)
     {
+        _factory = factory;
+    }
+
+    [Theory]
+    [MemberData(nameof(TheoryGetAwardRangeShouldBeReturnCorrectContent))]
+    public async Task GetAwardRangeShouldBeReturnCorrectContent(AwardRangeResponse awardRangeResponse)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/movies/award-range");
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var contents = await response.Content.ReadAsStringAsync();
+        var resutl = JsonSerializer.Deserialize<AwardRangeResponse>(contents, options);
+
+        resutl.Should().BeEquivalentTo(
+            awardRangeResponse,
+            opt => opt.IncludingProperties());
 
     }
 }
